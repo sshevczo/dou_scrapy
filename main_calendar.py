5199942f4ce066d4b38fb615f894c470743d09e8
Maryna Mykhailiak <marinamihajlak09@gmail.com>
2024-02-16 17:50:08 +0200

    the code parses all events

--- main_calendar.py
+++ main_calendar.py
@@ -3,21 +3,28 @@ import requests
 from time import sleep 
 import csv
 
-url = 'https://dou.ua/calendar/page-1/'
-
 headers = {
     'Accept': '*/*',  
     'User-Agent' :'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36'
 }
 
+# possible rows names
 
-with open('result/result.csv', 'w', encoding='utf-8') as file:
+first_row_name = ['Відбудеться', 'Відбулось', 'Took place', 'Состоялось', 'Date']
+second_row_name = ['Початок', 'Time', 'Час', 'Начало', 'Время']
+third_row_name = ['Місце', 'Place', 'Место']
+fourth_row_name = ['Вартість', 'Price', 'Стоимость']
+fifth_row_name = ['Підуть', 'Attendees', 'Пойдут']
+
+all_row_name = [first_row_name, second_row_name, third_row_name, fourth_row_name]
+
+with open('result/result2.csv', 'w', encoding='utf-8') as file:
     writer = csv.writer(file)
     writer.writerow(
         [
             'Event name',
             'Image',
-            'Day',
+            'Date',
             'Time',
             'Place',
             'Price',
@@ -26,69 +33,114 @@ with open('result/result.csv', 'w', encoding='utf-8') as file:
             'Views count'
         ]
      )
-    
+
 
 def get_info(src):
     soup_each_event = BeautifulSoup(src, 'lxml')
     title = soup_each_event.find('div', class_ = 'page-head').find('h1').text
 
-    img = soup_each_event.find('img', class_ = 'event-info-logo').get('src')
+    try:
+        img = soup_each_event.find('img', class_ = 'event-info-logo').get('src')
+    except Exception:
+        img = ''
 
     list_event_info_row = soup_each_event.find_all('div', class_ = 'event-info-row')
 
     list_people_links = []
     res = []
+    value_dict = {}
     for row in list_event_info_row:
-        each_property =  row.find(class_ = 'dt').text.strip()
-        if (len(list_event_info_row) == 4 and list_event_info_row.index(row) == 3)\
-        or (len(list_event_info_row) == 5 and list_event_info_row.index(row) == 4):
+        value_dict[row.find(class_ = 'dt').text.strip()] = row.find(class_ = 'dd').text.strip()
+    
+    for row_name in all_row_name:
+        value_for_res = ''
+        for name in row_name:
+            if name in value_dict:
+                value_for_res = value_dict[name]
+
+        res.append(value_for_res)
+
+    last_item = value_dict.popitem()
+    if '\n' in last_item[0]:
+        main_name_row = last_item[0].split('\n')[0].strip()
+        if main_name_row in fifth_row_name:
             people = row.find('div', id = "people").find_all('a')
+
             for person in people[:-1]:
                 list_people_links.append(person.get('href'))
             res.append(list_people_links)
 
-        else:
-            value = row.find(class_ = 'dd').text.strip()
-            res.append(value)
+    elif last_item[0] in fifth_row_name:
+        people = row.find('div', id = "people").find_all('a')
 
-    if len(res) == 4 or len(res) == 3:
-        res.insert(1, '')
+        for person in people[:-1]:
+            list_people_links.append(person.get('href'))
+        res.append(list_people_links)
+    else:
+        res.append('')
 
-    list_tags = soup_each_event.find('div', class_ = 'b-post-tags').find_all('a')
-    tags = [t.text for t in list_tags]
+    try:
+        list_tags = soup_each_event.find('div', class_ = 'b-post-tags').find_all('a')
+        tags = [t.text for t in list_tags]
+    except Exception:
+        tags = ''
 
-    page_views_title = soup_each_event.find('div', class_ = 'b-post-tags').find('span', class_ = 'pageviews').get('title')
     page_views_count = soup_each_event.find('div', class_ = 'b-post-tags').find('span', class_ = 'pageviews').text
 
-    with open(f'result/result.csv', 'a', encoding='utf-8', newline='') as file:
+    with open(f'result/result2.csv', 'a', encoding='utf-8', newline='') as file:
         writer = csv.writer(file)
         writer.writerow (
             (
                 title,
                 img,     
-                res[0],  #day
-                res[1],  #time
-                res[2],  #place
-                res[3],  #price
-                ', '.join(res[4]) if len(res) == 5 else '', #attendees
+                res[0],  # d]ay
+                res[1],  # time
+                res[2],  # place
+                res[3],  # price
+                ', '.join(res[4]) if len(res) == 5 else '', # attendees
                 ', '.join(tags),
                 page_views_count
-           )
         )
-    print('SUCCESS')
-    sleep(1)
+        )
+
+    print('success')
+    sleep(0.1)
+
+def pars_all_pages(url):
+    req_main = requests.get(url, headers=headers)
+    soup_main = BeautifulSoup(req_main.text, 'lxml')
+
+    all_cards = soup_main.find_all(class_ = 'b-postcard') 
+
+    for card in all_cards: 
+        url_each_event = card.find('h2', class_ = 'title').find('a').get('href')
+        req_each_event = requests.get(url_each_event, headers=headers)
+
+        get_info(req_each_event.text)
 
 
-req_main = requests.get(url, headers=headers)
-soup_main = BeautifulSoup(req_main.text, 'lxml')
+def get_count_page():
+    
+    # automatic calculation of pages in the archive
 
-all_cards = soup_main.find_all(class_ = 'b-postcard') 
+    # req_archive = requests.get(url, headers=headers)
+    # soup_archive = BeautifulSoup(req_archive.text, 'lxml')
+    # count_archive_page = int(soup_archive.find(class_ = 'b-paging').find_all(class_ = 'page')[-1].text)
+    # for num in range(1, count_archive_page+1):
+    #     pars_all_pages(f'https://dou.ua/calendar/archive/{num}/')
+    
+    for num in range(50, 0, -1):   # number of pages in archive
+        pars_all_pages(f'https://dou.ua/calendar/archive/{num}/')
 
-for card in all_cards: 
-    url_each_event = card.find('h2', class_ = 'title').find('a').get('href')
-    req_each_event = requests.get(url_each_event, headers=headers)
+    url = 'https://dou.ua/calendar'
 
-    get_info(req_each_event.text)
+    req = requests.get(url, headers=headers)
+    soup = BeautifulSoup(req.text, 'lxml')
+    count_page = int(soup.find(class_ = 'b-paging').find_all(class_ = 'page')[-1].text)
 
+    for num in range(1, count_page+1):
+        pars_all_pages(f'https://dou.ua/calendar/page-{num}/')
+
+get_count_page()
 
 
